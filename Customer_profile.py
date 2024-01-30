c1363b9de95f3b83e3f93cfeab54945edac6edbc
Carl GENNETAIS <carl.gennetais@pm.me>
2024-01-30 20:33:18 +0100

    changed string concats to fstrings

--- Customer_profile.py
+++ Customer_profile.py
@@ -3,12 +3,14 @@ Main Frontend code using streamlit
 """
 # import pickle
 
+import numpy as np
 import pandas as pd
 import requests
+import shap
 import streamlit as st
 
 # API params
-BACK_URL = "http://127.0.0.1:8000"
+API_URL = "http://127.0.0.1:8000"
 
 # Page params
 st.set_page_config(
@@ -18,35 +20,50 @@ st.set_page_config(
 st.title("Home Credit Loan Validation")
 
 
-# response = requests.get(BACK_URL)
+# response = requests.get(API_URL)
 # st.write(response.json()[0])
 
 
 # Choose from customer list
 with st.sidebar:
-    customer_list = requests.get(BACK_URL + "/customers").json()
+    customer_list = requests.get(f"{API_URL}/customers").json()
     customer_list.insert(0, "All")
     selectedID = st.selectbox("Select customer", customer_list)
 
 if str(selectedID) == "All":
     # All customers stats
     st.header("All customers stats")
-    all_customers_stats = requests.get(BACK_URL + "/customers_stats/").json()
+    all_customers_stats = requests.get("{API_URL}/customers_stats/").json()
     st.dataframe(all_customers_stats)
-else:
-    # Customer profile
-    st.header("Profile of customer " + str(selectedID))
-    customer_profile = requests.get(BACK_URL + "/customers/" + str(selectedID)).json()
-    st.dataframe(customer_profile)
+    with st.sidebar:
+        st.metric("Customer count", len(customer_list) - 1)
 
+else:
     # Predict for one customer
-    st.header("Scoring for customer " + str(selectedID))
-    predict = requests.get(BACK_URL + "/predict/" + str(selectedID)).json()
+    st.header(f"Loan status for customer {str(selectedID)}")
+    predict = requests.get(f"{API_URL}/predict/{str(selectedID)}").json()
     st.table(predict)
     st.success("Loan granted", icon="✅")
     st.error("Loan denied", icon="❌")
 
+    # Customer profile
+    st.header(f"Profile of customer {str(selectedID)}")
+    with st.expander("Display full profile"):
+        customer_profile = requests.get(f"{API_URL}/customers/{str(selectedID)}").json()
+        st.dataframe(customer_profile)
+
     # Shap values
-    st.header("Shap values for customer " + str(selectedID))
-    shap = requests.get(BACK_URL + "/shap/" + str(selectedID)).json()
-    st.write(shap)
+    st.header(f"Shap values for customer {str(selectedID)}")
+    shap_response = requests.get(f"{API_URL}/shap/{str(selectedID)}").json()
+    # st.write(shap)
+    shap_dict = shap_response
+    # merge top and bottom into one dict
+    shap_dict["top"].update(shap_dict["bottom"])
+    keys = np.fromiter(shap_dict["top"].keys(), dtype=object)
+    values = np.fromiter(shap_dict["top"].values(), dtype=float)
+    fig = shap.bar_plot(
+        values,
+        feature_names=keys,
+        max_display=20,
+    )
+    st.pyplot(fig=fig)
